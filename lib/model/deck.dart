315496578dfd4453f4f5bb4ee1228b996e3e069c
Magffor <133989969+Magffor@users.noreply.github.com>
2024-01-23 01:03:38 +0100

    23 - 01

---
+++ lib/model/deck.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'piece.dart';
+
+class Deck {
+  // ATTRIBUTS
+
+  final List<Piece> _pieces;
+
+  // CONSTRUCTEUR
+
+  Deck(piece) : _pieces = [piece];
+
+  // REQUETES
+
+  List<Piece> get pieces => _pieces;
+  Piece getPiece(int index) => _pieces[index];
+
+  Widget getWidget(){
+    return ListView.builder(
+      itemCount: _pieces.length,
+      itemBuilder: (context, index){
+        return _pieces[index].getWidget();
+      }
+    );
+  }
+  // COMMANDES
+
+  void addPiece(Piece piece) {
+    _pieces.add(piece);
+  }
+
+  void addPieces(List<Piece> pieces){
+    _pieces.addAll(pieces);
+  }
+
+  void removePiece(Piece piece) {
+    if(_pieces.contains(piece)){
+      _pieces.remove(piece);
+    } else {
+      throw Exception("La pièce n'est pas dans le deck");
+    }
+  }
+
+  void removeAllPieces(){
+    _pieces.clear();
+  }
+
+  void removePieceAt(int index) {
+    _pieces.removeAt(index);
+  }
+
+}
